3147b9a11a497e99fdf16b068354955b29942d7f
ErikKnops <94675426+ErikKnops@users.noreply.github.com>
2022-06-07 09:14:20 +0200

    wat kleine veranderingen

--- One-Up/index.php
+++ One-Up/index.php
@@ -1,5 +1,5 @@
-<?php 
-require("php/functies.php"); 
+<?php
+require("php/functies.php");
 ?>
 
 <!DOCTYPE html>
@@ -9,13 +9,14 @@ require("php/functies.php");
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="One-Up">
-  <meta name="author" content="Jesca Wiegers">
+  <meta name="author" content="Jesca Wiegers, Danny van Kampen, Erik Knöps, Sami Alouat">
   <meta name="keywords" content="">
   <link href="https://fonts.googleapis.com/css2?family=Tourney:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="css/master.css">
   <link rel="stylesheet" type="text/css" href="css/index.css">
   <link rel="stylesheet" type="text/css" href="css/slideshow.css">
+  <link rel="icon" type="image/x-icon" href="images/favicon.ico">
   <script type="text/javascript" src="js/burger-menu.js"></script>
   <title>One-Up</title>
 </head>
