dd5c889b546fbbeefb478a6a52243cd3a68bf60a
ErikKnops <94675426+ErikKnops@users.noreply.github.com>
2022-06-13 12:27:00 +0200

    bewerken toegevoegt

--- One-Up/evenementen.php
+++ One-Up/evenementen.php
@@ -9,13 +9,12 @@ require("php/functies.php");
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="One-Up">
-  <meta name="author" content="Jesca Wiegers, Danny van Kampen, Erik Knöps, Sami Alouat">
+  <meta name="author" content="Jesca Wiegers">
   <meta name="keywords" content="">
   <link href="https://fonts.googleapis.com/css2?family=Tourney:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="css/master.css">
   <link rel="stylesheet" type="text/css" href="css/evenementen.css">
-  <link rel="icon" type="image/x-icon" href="images/favicon.ico">
   <script type="text/javascript" src="js/burger-menu.js"></script>
   <title>One-Up</title>
 </head>
@@ -26,6 +25,28 @@ require("php/functies.php");
   <main class="container">
 
     <div class="extra-container">
+      <?php
+        require('dbconnect.php');
+
+        // $username = trim($_SESSION['user']);
+        // $password = trim($_SESSION['pass']);
+
+        $sql = "SELECT * FROM gebruikers WHERE username = 'admin'";
+
+        if ($result = $conn->query($sql)) {
+          $rowresult = $result->fetch_row();
+          $toestemming = $rowresult[3];
+
+          if ($toestemming >= 2) {
+            ?> <a class="bewerken-button" href="bewerken.php">Evenementen Bewerken</a> <?php
+            $result->close();
+            echo "toestemming";
+          } else {
+            $result->close();
+            echo "geen toestemming";
+          }
+        }
+      ?>
 
       <h1 class="pagina-titel">Komende Evenementen</h1>
 
--- One-Up/index.php
+++ One-Up/index.php
@@ -9,14 +9,13 @@ require("php/functies.php");
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="One-Up">
-  <meta name="author" content="Jesca Wiegers, Danny van Kampen, Erik Knöps, Sami Alouat">
+  <meta name="author" content="Jesca Wiegers">
   <meta name="keywords" content="">
   <link href="https://fonts.googleapis.com/css2?family=Tourney:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="css/master.css">
   <link rel="stylesheet" type="text/css" href="css/index.css">
   <link rel="stylesheet" type="text/css" href="css/slideshow.css">
-  <link rel="icon" type="image/x-icon" href="images/favicon.ico">
   <script type="text/javascript" src="js/burger-menu.js"></script>
   <title>One-Up</title>
 </head>
@@ -27,18 +26,45 @@ require("php/functies.php");
   <main class="container">
 
     <div class="slideshow-container">
+      <?php
+        require('dbconnect.php');
+      ?>
       <h1 class="slideshow-title">Huidige Aanbiedingen</h1>
       <button class="slideshow__button slideshow__button--left" onclick="plusSlides(-1, 0)">
         <img src="images/master/left.png">
       </button>
       <div class="mySlides1">
-        <img class="slideshow-image" src="images/index/aanbieding-1.png" alt="">
+        <?php
+          $sql = "SELECT * FROM aanbiedingen WHERE aanbiedingen_id = 1";
+
+          if ($result = $conn->query($sql)) {
+            while ($row = $result->fetch_assoc()) {
+              ?> <img src="images/aanbiedingen/<?php echo $row['afbeelding'];?>" alt=""><?php
+            }
+          }
+        ?>
       </div>
       <div class="mySlides1">
-        <img class="slideshow-image" src="images/index/aanbieding-2.png" alt="">
+        <?php
+          $sql = "SELECT * FROM aanbiedingen WHERE aanbiedingen_id = 2";
+
+          if ($result = $conn->query($sql)) {
+            while ($row = $result->fetch_assoc()) {
+              ?> <img src="images/aanbiedingen/<?php echo $row['afbeelding'];?>" alt=""><?php
+            }
+          }
+        ?>
       </div>
       <div class="mySlides1">
-        <img class="slideshow-image" src="images/index/aanbieding-3.png" alt="">
+        <?php
+          $sql = "SELECT * FROM aanbiedingen WHERE aanbiedingen_id = 3";
+
+          if ($result = $conn->query($sql)) {
+            while ($row = $result->fetch_assoc()) {
+              ?> <img src="images/aanbiedingen/<?php echo $row['afbeelding'];?>" alt=""><?php
+            }
+          }
+        ?>
       </div>
       <button class="slideshow__button slideshow__button--right" onclick="plusSlides(1, 0)">
         <img src="images/master/right.png" alt="">
@@ -51,15 +77,39 @@ require("php/functies.php");
         <img src="images/master/left.png">
       </button>
       <div class="mySlides2">
-        <img class="slideshow-image" src="images/index/evenement-1.png" alt="">
+        <?php
+          $sql = "SELECT * FROM aanbiedingen WHERE aanbiedingen_id = 7";
+
+          if ($result = $conn->query($sql)) {
+            while ($row = $result->fetch_assoc()) {
+              ?> <img src="images/aanbiedingen/<?php echo $row['afbeelding'];?>" alt=""><?php
+            }
+          }
+        ?>
         <a class="slideshow__details" href="#">Details</a>
       </div>
       <div class="mySlides2">
-        <img class="slideshow-image" src="images/index/evenement-2.png" alt="">
+        <?php
+          $sql = "SELECT * FROM aanbiedingen WHERE aanbiedingen_id = 6";
+
+          if ($result = $conn->query($sql)) {
+            while ($row = $result->fetch_assoc()) {
+              ?> <img src="images/aanbiedingen/<?php echo $row['afbeelding'];?>" alt=""><?php
+            }
+          }
+        ?>
         <a class="slideshow__details" href="#">Details</a>
       </div>
       <div class="mySlides2">
-        <img class="slideshow-image" src="images/index/evenement-3.png" alt="">
+        <?php
+          $sql = "SELECT * FROM aanbiedingen WHERE aanbiedingen_id = 5";
+
+          if ($result = $conn->query($sql)) {
+            while ($row = $result->fetch_assoc()) {
+              ?> <img src="images/aanbiedingen/<?php echo $row['afbeelding'];?>" alt=""><?php
+            }
+          }
+        ?>
         <a class="slideshow__details" href="#">Details</a>
       </div>
       <button class="slideshow__button slideshow__button--right" onclick="plusSlides(1, 1)">
